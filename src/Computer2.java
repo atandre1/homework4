3f86c0b824d922b84ed9b7d91bbdf272fd6669e8
atandre1 <atandrei@protonmail.com>
2022-07-05 18:50:52 +0300

    add cycle

--- src/Computer2.java
+++ src/Computer2.java
@@ -1,9 +1,10 @@
 public class Computer2 {
     public static void main(String[] args) {
         Computer MacBook = new Computer("M2", 24000, 2000, 2);
-
-        MacBook.printInfo();
-        MacBook.turnON();
-        MacBook.turnOFF();
+        for (int j = 0; j < 100; j++){
+            //MacBook.printInfo();
+            MacBook.turnON();
+            MacBook.turnOFF();
+        }
     }
 }
